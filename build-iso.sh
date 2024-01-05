a38ca9d015f4861034be04c63eeb269aa8632a5b
MikuX-Dev <abinishanth3464@gmail.com>
2024-01-05 18:44:12 +0530

    done

--- build-iso.sh
+++ build-iso.sh
@@ -2,31 +2,8 @@
 
 set -e
 
-packages_file="./profile/aur_package"
-output_dir="./aur_packages"
+echo "This script is not ready"
+exit
 
-output_dir="$(readlink -f "$output_dir")"
-
-mkdir -p "$output_dir"
-
-while read -r package; do
-  git clone "https://aur.archlinux.org/$package.git"
-  cd "$package"
-  makepkg -sf --noconfirm --needed --noprogressbar
-  # shellcheck disable=SC2035
-  cp *.pkg.tar.* "$output_dir"
-  cd ..
-done <"$packages_file"
-
-mkdir -p /tmp/archlive
-cp -r . /tmp/archlive
-
-pacman --noconfirm -S archiso
-
-find /tmp/archlive
-cd /tmp/archlive
-
-sed -i "s#^Server = file:///home/custompkgs#Server = file://${output_dir}#" ./profile/pacman.conf
-
-mkdir {work,out}
-mkarchiso -v -w work/ -o out/ ./profile
+# pacman --noconfirm -S archiso mkinitcpio-archiso
+# mkarchiso -v -w /tmp/work/ -o /home/builder/out/ ./profile
--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="alg-xfce"
-iso_label="ALG_XFCE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="DemonKiller <https://demonkiller.tech>"
-iso_application="ALG Live/Rescue CD"
-iso_version="$(date +%Y.%m)"
+iso_name="archfiery-xfce"
+iso_label="ARCHFIERY_XFCE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%d%m%Y)"
+iso_publisher="MikuX-Dev <https://github.com/MikuX-Dev>"
+iso_application="ARCHFIERY Live/Installation/Rescue CD"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%d.%m.%Y)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
@@ -17,8 +17,13 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:0400"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/skel/bin/.*"]="0:0:755"
+  ["/etc/skel/.local/share/fish/generated_completions/.*"]="0:0:755"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/customize_airootfs.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
